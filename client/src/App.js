032294de1f2dbb14d2906d35c75c43f7e7d398ab
RiT <iamsirid@gmail.com>
2018-11-06 08:28:37 +0700

    add markup login front-end (not finnised)

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
+import Login from "./components/Login";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">Working in back-end...</header>
+        <div className="container">
+          <Login />
+        </div>
       </div>
     );
   }
