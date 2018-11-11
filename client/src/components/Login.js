6b7ebd6f1dc1d9a27a51de74c8a3721e26b560b8
RiT <iamsirid@gmail.com>
2018-11-11 17:37:12 +0700

    Add real login database check

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,23 +1,48 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as actionTypes from "../store/actions";
+import axios from "axios";
 
 class Login extends Component {
-  // onLoginClick = () => {
-  //   console.log("onLoginClick");
-  //   this.props.onLogin();
-  // };
   state = {
-    loginInput: ""
+    loginInput: "",
+    loginError: null
+  };
+
+  onLoginClick = () => {
+    console.log("onLoginClick");
+    if (!this.state.loginInput) return;
+    const data = {
+      ssn: this.state.loginInput,
+      role: "seller"
+    };
+    axios
+      .post("/api/login", data)
+      .then(response => {
+        console.log(response.data);
+        this.props.doLogin(this.state.loginInput);
+        this.setState({ loginError: null });
+      })
+      .catch(err => {
+        console.log(err.response.data);
+        this.setState({ loginError: err.response.data.errMsg });
+      });
+
+    // this.props.doLogin(this.state.loginInput);
   };
   onLoginInputChanged = e => {
     console.log(e.target.value);
     this.setState({ loginInput: e.target.value });
   };
+
   render() {
     let loginInfo = null;
     if (this.props.reduxState.isLogin) {
       loginInfo = <h1>Login as {this.props.reduxState.ssn}</h1>;
+    }
+    let loginErrorShow = null;
+    if (this.state.loginError) {
+      loginErrorShow = <h1>{this.state.loginError}</h1>;
     }
     return (
       <React.Fragment>
@@ -30,17 +55,15 @@ class Login extends Component {
             onChange={this.onLoginInputChanged}
           />
         </div>
-        <button
-          onClick={() => this.props.onLogin(this.state.loginInput)}
-          className="btn btn-primary"
-        >
+        <button onClick={this.onLoginClick} className="btn btn-primary">
           Login
         </button>
-        <button onClick={this.props.onLogout} className="btn btn-primary">
+        <button onClick={this.props.doLogout} className="btn btn-primary">
           Logout
         </button>
 
         {loginInfo}
+        {loginErrorShow}
       </React.Fragment>
     );
   }
@@ -54,12 +77,12 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onLogin: ssn =>
+    doLogin: ssn =>
       dispatch({
         type: actionTypes.LOGIN,
         ssn: ssn
       }),
-    onLogout: () => dispatch({ type: actionTypes.LOGOUT })
+    doLogout: () => dispatch({ type: actionTypes.LOGOUT })
   };
 };
 
---
+++ routes/api/login.js
@@ -0,0 +1,26 @@
+2;
+const express = require("express");
+
+const router = express.Router();
+module.exports = db => {
+  router.post("/", (req, res) => {
+    let loginInfo = req.body;
+
+    let sql = `SELECT ssn FROM ${loginInfo.role} WHERE ssn = ${loginInfo.ssn}`;
+    let query = db.query(sql, (err, result) => {
+      if (err) {
+        res.status(404).json(err);
+        return;
+      }
+      console.log(result);
+      //   res.send("seller login...");
+      if (result.length > 0) {
+        res.json(result[0]);
+      } else {
+        res.status(404).json({ errMsg: `No ${loginInfo.role} with this SSN` });
+      }
+    });
+  });
+
+  return router;
+};
